e956e04bf92801194184b961be5b04329e37e504
Thiago <thiagoc.carboneri@gmail.com>
2021-11-25 15:37:21 -0300

    exercicio pratico do dia

---
+++ bloco_19/dia_1/hello-world/index.js
@@ -0,0 +1,4 @@
+const { question, questionInt } = require('readline-sync');
+const name = question('Qual seu nome?');
+const age = questionInt('Qual sua idade?');
+console.log(`Hello, ${name}!. You are ${age} years old!`);
