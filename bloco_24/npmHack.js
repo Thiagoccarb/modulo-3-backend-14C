ae4199ce11bbe9a10bf36487e921161ca97c4243
Thiago <thiagoc.carboneri@gmail.com>
2022-01-21 16:10:05 -0300

    npm hack archive

---
+++ bloco_24/npmHack.js
@@ -0,0 +1,13 @@
+// ----Sequelize Hack ----
+
+// npm init -y
+// npm i express nodemon sequelize mysql2
+// npm i sequelize-cli
+// npx sequelize-cli init
+
+
+// ---- Sequelize hack for data changing ----
+
+// npx sequelize db:migrate:undo:all
+// npx sequelize db:migrate
+// npx sequelize db:seed:all
