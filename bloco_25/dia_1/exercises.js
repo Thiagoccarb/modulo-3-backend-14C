ea416770531aa948c3338774afdcddb3e4185fa6
Thiago <thiagoc.carboneri@gmail.com>
2022-02-08 22:04:18 -0300

    updating branch

--- bloco_25/dia_1/exercises.js
+++ bloco_25/dia_1/exercises.js
@@ -12,14 +12,3 @@
 // heroku create my-first-app-1230 --remote prod
 // heroku config:set TEXT="prod" --app my-first-deploy-1230
 // git push prod master;
-
-
-
-// # Boas vindas ao repositório do projeto Loto!
-// Trata-se de um simulador de jogos da lotofácil e megasena, com a opção de checagem dos históricos de sorteios ou último sorteio. 
-
-// # Funcionalidades
-//   - É possível escolher qual tipo de loteria quer simular, lotofácil ou megasema.
-//   - Os números são escolhidos através de uma cartela que simula as cartelas reais, para escolher os números basta clicá-los.
-//   - É possível verificar a performance histórica nos sorteios anteriores ao verificar a quantidade de acertos da loteria simulada.
-//   - Aplicativo utiliza Context e Hooks para gerenciamento de estado. 
