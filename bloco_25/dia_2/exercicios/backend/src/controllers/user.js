3c4b3aa235a20066c93efa8c07d2b77c14645360
Thiago <thiagoc.carboneri@gmail.com>
2022-02-09 22:19:30 -0300

    fixing api bugs

--- bloco_25/dia_2/exercicios/backend/src/controllers/user.js
+++ bloco_25/dia_2/exercicios/backend/src/controllers/user.js
@@ -2,5 +2,6 @@ const { User } = require('../models');
 
 module.exports = async (_req, res) => {
     const users = await User.findAll();
+    console.log(users);
     res.status(200).json(users);
 };
